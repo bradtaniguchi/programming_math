7eb31ab06c672eb4ae351bf5ed82c2f7ed652811
Brad <bradtaniguchi@gmail.com>
2016-08-23 16:54:17 -0700

    Fixed spacing, tabs, minor logic changes

--- src/Birthday.java
+++ src/Birthday.java
@@ -1,37 +1,31 @@
 import java.util.Scanner;
 public class Birthday {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-Scanner input = new Scanner(System.in);
-int answer, numP=0, days=365;
-double prob=1, inputP;
-
-do{
-	do{	
-		System.out.print("\nEnter a prob - at least 2 people share the same Bday:");
-		inputP=input.nextDouble();
-	  }while(inputP<=0||inputP>=1.0);
-	
-	System.out.println("Calculation section:\n------------------");
-	for(;days>0;){
-		numP++;
-		
-		prob*=days/365.0;
-		
-		System.out.println(days+" :"+prob);
-		days--;
-		if(1-prob>inputP)
-			break;
-				 }
-	System.out.println("------------------");
-	System.out.println(numP+" people are required");
-	System.out.println("The prob that two or more have the same birthday is "+(1-prob));
-	System.out.print("\nRun again? 1 for yes, any other number for no: ");
-	answer=input.nextInt();
-}while(answer==1);
-		
-	}
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int answer, numP=0, days=366;
+        double prob=1, inputP;
+        
+        do {
+            do { 
+                System.out.print("\nEnter a prob - at least 2 people share the same Bday:");
+                inputP=input.nextDouble();
+            } while(inputP<=0||inputP>=1.0);
+            
+            System.out.println("Calculation section:\n------------------");
+            while(days>0) {
+                numP++;
+                prob *= days/365.0;
+                System.out.println(days + " :" + prob);
+                days--;
+                if(1-prob>inputP)
+                    break;
+            }
+            System.out.println("------------------");
+            System.out.println(numP + " people are required");
+            System.out.println("The prob that two or more have the same birthday is " + (1-prob));
+            System.out.print("\nRun again? 1 for yes, any other number for no: ");
+            answer=input.nextInt();
+        } while(answer == 1);
+    }
 }
